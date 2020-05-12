7527b34d22c68d369b320fa725a2af3cf2d13091
Thea Ganden <wtp9998@hotmail.com>
2020-05-12 09:47:10 -0600

    format spots to match design

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,6 +3,17 @@ import "components/DayListItem.scss";
 import classnames from "classnames";
 
 export default function DayListItem(props) {
+	const formatSpots = function(props) {
+		if (props.spots === 0) {
+			return ("no spots remaining")
+		} else if (props.spots === 1) {
+			return ("1 spot remaining")
+		} else {
+			return (props.spots + " spots remaining")
+		}
+		
+	};
+
 	const dayClass = classnames("day-list__item", {
 		"day-list__item--selected": props.selected,
 		"day-list__item--full": props.spots === 0
@@ -11,7 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
