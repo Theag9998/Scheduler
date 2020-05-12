13354bd3a636f8d1080b7509868575b373957ca7
Thea Ganden <wtp9998@hotmail.com>
2020-05-11 22:03:12 -0600

    Create DayListItem

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "components/Button.scss";
-import classNames from "classnames"
+import classnames from "classnames"
 
 export default function Button(props) {
-   const buttonClass = classNames("button", {
+   const buttonClass = classnames("button", {
       " button--confirm": props.confirm,
       " button--danger": props.danger
    });
