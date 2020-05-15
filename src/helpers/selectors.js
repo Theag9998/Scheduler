fb73b3936c093e900eaffa77f32501c51dc7b86f
Thea Ganden <wtp9998@hotmail.com>
2020-05-15 14:05:59 -0600

    Create helper getAppointmentsForDay

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+
+export function getAppointmentsForDay(state, day) {
+	const filteredDays = state.days.filter( (days) => days.name === day)
+	if (!filteredDays.length) {
+		return []
+	}
+	const appointmentIds = (filteredDays[0].appointments)
+	const appointments = appointmentIds.map( (id) => {
+		return state.appointments[id] 
+
+	})
+
+	return appointments
+	
+}
